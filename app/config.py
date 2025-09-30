356dd64242afbf4f26290d0b5762f17ef928d3da
Angel Espinoza <angelezpin@MacBook-Air-de-Angel.local>
2025-09-30 17:41:15 -0600

    Subiendo backend a GitHub

---
+++ app/config.py
@@ -0,0 +1,15 @@
+# app/config.py
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    db_host: str = "estrategia.dnsdojo.com"
+    db_port: str = "5432"
+    db_name: str = "mibasededatos"
+    db_user: str = "miusuario"
+    db_password: str = "micontrasena"
+    cors_origins: str = "http://localhost:3000,http://127.0.0.1:3000"
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
