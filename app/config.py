0f3b1e0a4509c31cd4fe018c0fe74c4c23c76e91
aezpinozarr <aezpinozarr@gmail.com>
2025-10-01 09:04:01 -0600

    Update config.py

--- app/config.py
+++ app/config.py
@@ -7,7 +7,11 @@ class Settings(BaseSettings):
     db_name: str = "mibasededatos"
     db_user: str = "miusuario"
     db_password: str = "micontrasena"
-    cors_origins: str = "http://localhost:3000,http://127.0.0.1:3000"
+    cors_origins: str = (
+        "http://localhost:3000,"
+        "http://127.0.0.1:3000,"
+        "https://my-dashboard-production-ecd1.up.railway.app"
+    )
 
     class Config:
         env_file = ".env"
